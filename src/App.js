2a5a3f82bd29758862ea8b6bbd825112e2cb5d54
Kapil <68738855+kapil2020@users.noreply.github.com>
2024-12-01 18:37:58 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+import SurveyIntro from "./components/survey_sections/general/Intro";
+
+function App() {
+  return (
+    <div className="">
+      <header className=""></header>
+      <body>
+        <SurveyIntro />
+      </body>
+    </div>
+  );
+}
+
+export default App;
